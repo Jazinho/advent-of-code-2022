54727ceacbc62a08e1c6b8eac546a48607dc2488
jpalucki <jan.palucki@miquido.com>
2022-12-06 13:47:27 +0100

    day 6 solution - detecting packet/message start markers

---
+++ Day 6/src/com/jpalucki/Day6Main.java
@@ -0,0 +1,35 @@
+package com.jpalucki;
+
+import java.util.*;
+
+public class Day6Main {
+
+  private final static int MAX_PACKET_STARTER_BUFFER_SIZE = 14;
+  private final static int MAX_MESSAGE_BUFFER_SIZE = 14;
+
+  public static void main(String[] args) {
+    String INPUT = InputProvider.getInput();
+    LinkedList<String> checkBuffer = new LinkedList<String>();
+    String[] chars = INPUT.split("");
+    for (int i = 0; i < chars.length; i++) {
+      if(checkBuffer.size() == MAX_MESSAGE_BUFFER_SIZE) {
+        if (containsMessageStartMarker(checkBuffer)) {
+          System.out.println("Found marker position at index: " + i);
+          break;
+        } else {
+          checkBuffer.pop();
+        }
+      }
+
+      checkBuffer.add(chars[i]);
+    }
+  }
+
+  private static boolean containsPacketStartMarker(List<String> list) {
+    return new HashSet<String>(list).size() == MAX_PACKET_STARTER_BUFFER_SIZE;
+  }
+
+  private static boolean containsMessageStartMarker(List<String> list) {
+    return new HashSet<String>(list).size() == MAX_MESSAGE_BUFFER_SIZE;
+  }
+}
