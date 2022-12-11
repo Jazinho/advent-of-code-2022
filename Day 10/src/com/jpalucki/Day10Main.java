720a165355584398c184ce7a04a42ef6028042ff
jpalucki <jan.palucki@miquido.com>
2022-12-11 11:41:08 +0100

    day 10 solution - device screen output handling

--- Day 10/src/com/jpalucki/Day10Main.java
+++ Day 10/src/com/jpalucki/Day10Main.java
@@ -1,48 +1,53 @@
 package com.jpalucki;
 
-import java.util.*;
-
 public class Day10Main {
 
   static final String ADDX_COMMAND = "addx";
   static final int ADD_COMMAND_CYCLES = 2;
-  static Set<Integer> DESIRED_CYCLES_SET = new HashSet<>(List.of(20, 60, 100, 140, 180, 220));
+  static final int SINGLE_ROW_PIXELS_COUNT = 40;
 
   public static void main(String[] args) {
     String INPUT = InputProvider.getInput();
     String[] rows = INPUT.split("\n");
-    int finalResult = 0;
-    int currentRegisterValue = 1;
+    int spritePos = 1;
     int currentCommandNo = 0;
-
-    for (int i = 1; i <= 220; i++) {
+    for (int crtPos = 1; true; crtPos++) {
       String command = rows[currentCommandNo];
 
       if (command.startsWith(ADDX_COMMAND)) {
         int nextAddedValue = Integer.parseInt(command.split(" ")[1]);
         for (int j = 1; j < ADD_COMMAND_CYCLES; j++) {
-          finalResult = checkCycle(i, currentRegisterValue, finalResult);
-          i++;
+          drawPixel(crtPos, spritePos);
+          crtPos++;
         }
 
-        finalResult = checkCycle(i, currentRegisterValue, finalResult);
-        currentRegisterValue += nextAddedValue;
+        drawPixel(crtPos, spritePos);
+        spritePos += nextAddedValue;
       } else {
-        finalResult = checkCycle(i, currentRegisterValue, finalResult);
+        drawPixel(crtPos, spritePos);
       }
 
       currentCommandNo++;
+      if (currentCommandNo == rows.length) {
+        return;
+      }
     }
-
-    System.out.println("Sum of cycles is: " + finalResult);
   }
 
-  private static int checkCycle(int cycleNo, int currentRegisterValue, int currentResult) {
-    if (DESIRED_CYCLES_SET.contains(cycleNo)) {
-      System.out.println(currentResult + " + (" + cycleNo + " * " + currentRegisterValue + ")");
-      return currentResult + (cycleNo * currentRegisterValue);
+  private static boolean isRowFinalPixel(int cycleNo) {
+    return cycleNo % SINGLE_ROW_PIXELS_COUNT == 0;
+  }
+
+  private static void drawPixel(int crtPos, int spritePos) {
+    crtPos = crtPos % SINGLE_ROW_PIXELS_COUNT;
+    if (crtPos - spritePos >= 0 && crtPos - spritePos <= 2) {
+      System.out.print("#");
     } else {
-      return currentResult;
+      System.out.print(".");
+    }
+
+    if (isRowFinalPixel(crtPos)) {
+      System.out.println();
     }
   }
 }
