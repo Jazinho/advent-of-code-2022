801526b1c23894e18ae9ed8b3fb5e01b365da1e8
jpalucki <jan.palucki@miquido.com>
2022-12-15 15:47:20 +0100

    day 15 - beacon position search (line search only)

--- Day 15/src/com/jpalucki/Day15Main.java
+++ Day 15/src/com/jpalucki/Day15Main.java
@@ -20,12 +20,13 @@ public class Day15Main {
       int sensorY = Integer.parseInt(outputParts[3].substring(2, outputParts[3].length() - 1));
       int beaconX = Integer.parseInt(outputParts[8].substring(2, outputParts[8].length() - 1));
       int beaconY = Integer.parseInt(outputParts[9].substring(2));
-      int radarRange = Math.abs(sensorX - beaconX) + Math.abs(sensorY - beaconY);
-      int sensorToLineDistance = sensorY - CHECKED_ROW_NO;
       sensorsInfo.put(Map.entry(sensorX, sensorY), Map.entry(beaconX, beaconY));
     }
 
-    sensorsScan(sensorsInfo, scannedIndexes);
+    int iterationCount = 4_000_000;
+    for (int i = 0; i < iterationCount; i++) {
+      sensorsScan(sensorsInfo, i, iterationCount);
+    }
     Set<Integer> sensorsOnLine = sensorsInfo.keySet().stream()
       .filter(sensor -> sensor.getValue().equals(CHECKED_ROW_NO))
       .map(Map.Entry::getKey)
@@ -43,19 +44,44 @@ public class Day15Main {
 
   private static void sensorsScan(
     Map<Map.Entry<Integer, Integer>, Map.Entry<Integer, Integer>> sensors,
-    Set<Integer> scannedIndexes
+    int lineNo,
+    int checkingLimit
   ) {
+    Set<Map.Entry<Integer, Integer>> ranges = new HashSet<>();
     for (Map.Entry<Map.Entry<Integer, Integer>, Map.Entry<Integer, Integer>> sensorInfo : sensors.entrySet()) {
       int sensorX = sensorInfo.getKey().getKey();
       int sensorY = sensorInfo.getKey().getValue();
       int beaconX = sensorInfo.getValue().getKey();
       int beaconY = sensorInfo.getValue().getValue();
       int radarRange = Math.abs(sensorX - beaconX) + Math.abs(sensorY - beaconY);
-      int relatedPointDistance = Math.abs(CHECKED_ROW_NO - sensorY);
+      int relatedPointDistance = Math.abs(lineNo - sensorY);
+      int maxValue = radarRange - relatedPointDistance;
 
-      for (int x = 0; x <= radarRange - relatedPointDistance; x++) {
-        scannedIndexes.add(sensorX + x);
-        scannedIndexes.add(sensorX - x);
+      if (maxValue >= 0) {
+        int minX = sensorX - maxValue;
+        int maxX = sensorX + maxValue;
+        ranges.add(Map.entry(minX, maxX));
+      }
+    }
+
+    List<Map.Entry<Integer, Integer>> orderedList = ranges.stream()
+      .sorted(Comparator.comparingInt(Map.Entry::getKey))
+      .collect(Collectors.toList());
+    int curMax = 0;
+    int curMin = checkingLimit;
+    for (int i = 0; i < orderedList.size() - 1; i++) {
+      int rangeMin = orderedList.get(i).getKey();
+      int rangeMax = orderedList.get(i).getValue();
+      if (rangeMin < curMin) {
+        curMin = rangeMin;
+      }
+      if (curMax < rangeMax) {
+        curMax = rangeMax;
+      }
+
+      int nextRangeMin = orderedList.get(i + 1).getKey();
+      if (nextRangeMin > curMax) {
+        System.out.println("line " + lineNo + " is a candidate");
       }
     }
   }
